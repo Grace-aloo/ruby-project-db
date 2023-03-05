582438e5bab709e86b188127be5304dd6f9b4183
grace_aloo <graceaaloo@gmail.com>
2023-03-05 11:44:12 +0300

    fixed limit on client side need to fix my fetch request

--- db/migrate/20230302094148_create_skills.rb
+++ db/migrate/20230302094148_create_skills.rb
@@ -3,8 +3,8 @@ class CreateSkills < ActiveRecord::Migration[7.0]
     create_table :skills do |t|
       t.string :name, limit:10
       t.string :tools 
-      t.integer :user_id  
-      # t.references :user, foreign_key: true    
+      # t.integer :user_id  
+      t.references :user, foreign_key: true    
       t.timestamps
     end
 
