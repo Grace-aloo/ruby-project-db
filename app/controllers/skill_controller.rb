e26e03cdb8d662199f682a912a54dd2a8c5b49f2
grace_aloo <graceaaloo@gmail.com>
2023-04-11 14:07:30 +0300

    trying to fix cors error

--- app/controllers/skill_controller.rb
+++ app/controllers/skill_controller.rb
@@ -5,7 +5,7 @@ class SkillController < AppController
         begin
           verify_auth # Verify authorization header and extract uid
         rescue StandardError => e
-          AppController.error_response(401, 'Unauthorized') # Return a 401 Unauthorized response
+          json_response(code:401,data:{error:'Unauthorized'}) # Return a 401 Unauthorized response
         end
       end
 
